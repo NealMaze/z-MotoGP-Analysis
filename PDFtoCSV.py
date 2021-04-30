4410612f0259ea26e9f05126f02fcca6d4cc886e
Neal Maze <nealwmaze@gmail.com>
2021-04-30 03:45:47 -0600

    y

--- PDFtoCSV.py
+++ PDFtoCSV.py
@@ -1,6 +1,8 @@
 # imports
 from ProcessPdfHelpers import *
-import PyPDF2 as p2
+# import PyPDF2 as p2
+import re
+import pdfplumber as plumb
 
 dir = ("C:/Users/LuciusFish/Desktop/MotoGP_PDFs/Analysis")
 
@@ -12,50 +14,57 @@ headers = ["Year", "Date", "League","TRK", "Track", "Session", "Track_Condition"
            "Lap_Valid", "Pit", "Lap_Time", "Section_1_Time", "Section_2_Time", "Section_3_Time", "Section_4_Time",
            "Section_5_Time", "Section_6_Time", "Section_7_Time", "Section_8_Time", "Avg_Speed"]
 
-rcFiles = getRaceFiles(yr, dir)
+pos = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th",
+       "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th",
+       "28th", "29th", "30th", "31st", "32nd", "33rd", "34th", "35th", "36th", "37th", "38th", "39th", "40th"]
 
-def parseRacPDF(file):
-    headers = ["Year", "Date", "League", "TRK", "Track", "Session", "Rider_Number", "Rider_Name", "Nationality",
-               "Team_Name", "Lap No.", "Lap_Valid", "Pit", "Lap_Time", "Section_1_Time", "Section_2_Time",
-               "Section_3_Time", "Section_4_Time", "Section_5_Time", "Section_6_Time", "Section_7_Time",
-               "Section_8_Time", "Avg_Speed"]
-    pg_count = 0
-    pdfFile = open(file, "rb")
-    read = p2.PdfFileReader(pdfFile)
-    pg = read.getPage(pg_count)
-    fst_page = (pg.extractText())
-    fst_page.strip("""TISSOT""")
+rcFiles = getRacAnalysis(yr, dir)
 
-    print("\n")
-    print(fst_page + "END")
+def parseRacAnalysis(rc_file):
+    pdf = plumb.open(rc_file)
+    pages = pdf.pages
+    const = getSessionConstants(pages)
+    race = []
+    rider_count = 0
+    text = []
 
-    year = ""
-    date = ""
-    league = ""
-    TRK = ""
-    track = ""
-    session = ""
-    laps = ""
 
-    # for page in read.getNumPages():
-    #     pg = read.getPage(pg_count)
-    #     pages.append(pg)
-    #     pg_count += 1
-    # for page in pages:
-        # strip boilerplate
-        # append to
 
-    # create dictionary
-    # create dataframe
-    # strip unnecessary opening boilerplate
-    # split by rider
-    # for rider: process, then append to dataframe
-    # return dataframe
 
-    # return pg_count
+
+
+
+
+
+    for pg in pages:
+        words = pg.extract_words()
+        for i in words:
+            text.append(i["text"])
+            if i["text"] in pos:
+                rider_count += 1
+        stripBoilerPlate(text)
+
+    rider = getRiderInfo(text, pos)
+    getLaps(text, const, rider, race)
+
+    pdf_num = 9
+    for tb in text[pdf_num:30]:
+        print(f"{pdf_num}     {tb}")
+        pdf_num += 1
+
+    for i in const:
+        print(i)
+
+    l = 1
+    for lap in race:
+        print(f"{l}   {lap}")
+        l += 1
+
+    print(f"Rider Count: {rider_count}")
+
 
 
 for file in rcFiles[0:1]:
     print(file)
 
-    parse = parseRacPDF(file)
+    parse = parseRacAnalysis(file)
--- ProcessPdfHelpers.py
+++ ProcessPdfHelpers.py
@@ -2,12 +2,132 @@
 from os import listdir
 import fnmatch
 
-
 # 1) Get File Helpers
-def getRaceFiles(yr, dir):
-    rcFiles = fnmatch.filter(listdir(dir), f"{yr}*RAC*nalysis.pdf")
-    new_files = [f"{dir}/{file}" for file in rcFiles]
-    return new_files
+def getRacAnalysis(yr, dir):
+    filter_files = fnmatch.filter(listdir(dir), f"{yr}*RAC*nalysis.pdf")
+    rc_files = [f"{dir}/{file}" for file in filter_files]
+    return rc_files
+
+def getQualAnalysis(yr, dir):
+    filter_files = fnmatch.filter(listdir(dir), f"{yr}*QP1*nalysis.pdf")
+    filter_two = fnmatch.filter(listdir(dir), f"{yr}*QP2*nalysis.pdf")
+    filter_three = fnmatch.filter(listdir(dir), f"{yr}*QP3*nalysis.pdf")
+    filter_four = fnmatch.filter(listdir(dir), f"{yr}*QP4*nalysis.pdf")
+    # todo
+
 
 # 2) Parse File Helpers
 
+
+
+# 3)
+def getIndex(lis, text):
+    index = lis.index(text)
+    return index
+
+def getRiderName(lis):
+    rid_name = lis[0] + " " + lis[1]
+    del lis[0:2]
+    return rid_name
+
+def getRiderNat(lis):###################################################################################################
+########################################################################################################################
+    ## create list of nationalities
+    ## while working line != list of nationalities:
+    ## append and remove team names
+########################################################################################################################
+########################################################################################################################
+    while lis[0]
+    nat = lis[0]
+    lis.remove(lis[0])
+    return nat
+
+def getSessionConstants(pages):
+    words = pages[0].extract_words()
+    sess_const = []
+
+    year = words[-5]["text"]
+    day = words[-6]["text"]
+    month = words[-7]["text"]
+
+    date = f"{month} {day} {year}"
+    sess_const.append(date)
+    sess_const.append(year)
+    TRK = words[9]["text"]
+    sess_const.append(TRK)
+    league = words[8]["text"]
+    sess_const.append(league)
+    session = words[14]["text"]
+    sess_const.append(session)
+    return sess_const
+
+def stripBoilerPlate(lis):
+    start_index = getIndex(lis, "Speed")
+    del lis[0:start_index]
+    end_index = getIndex(lis, "Fastest")
+    del lis[end_index:]
+
+def getRiderInfo(lis, pos):
+    x = 0
+
+    rid_info = []
+    rid_info.append(getRiderName(lis))
+    rid_info.append(getTeamName(lis))
+    rid_info.append(getRiderNat(lis))
+
+    while lis[x]["text"] != pos[0]:
+        x += 1
+
+    lis.remove(lis[x])
+    num = lis[x]["text"]
+    rid_info.insert(0, num)
+    lis.remove(lis[x])
+    lis.remove(lis[x])
+    lis.remove(lis[x])
+    lis.remove(lis[x])
+    lis.remove(lis[x])
+    str_laps = lis[x]["text"]
+    lis.remove(lis[x])
+    lis.remove(lis[x])
+    lis.remove(lis[x])
+
+    laps = str_laps.replace("laps=", "")
+    rid_info.insert(0, laps)
+
+    pos.remove(pos[0])
+    return rid_info
+
+def getLaps(lis, const, rider, race):
+    x = 1
+    y = int(rider[0])
+    rider.remove(rider[0])
+
+    for i in rider:
+        const.append(i)
+
+    for i in range(x, y):
+        lin = 9
+
+        while lis[lin]["text"] != str(x):
+            lin += 1
+
+        if lis[lin]["text"] == str(x):
+            lap_data = []
+            for i in const:
+                lap_data.append(i)
+            lap_data.append(x)                 # Lap Number
+            del lis[lin:lin+1]
+            lap_data.append(lis[lin]["text"])  # Lap Time
+            del lis[lin:lin+1]
+            lap_data.append(lis[lin]["text"])  # Sec1 Time
+            del lis[lin:lin+1]
+            lap_data.append(lis[lin]["text"])  # Sec2 Time
+            del lis[lin:lin+1]
+            lap_data.append(lis[lin]["text"])  # Sec3 Time
+            del lis[lin:lin+1]
+            lap_data.append(lis[lin]["text"])  # Sec4 Time
+            del lis[lin:lin+1]
+            lap_data.append(lis[lin]["text"])  # Lap Avg Speed
+            del lis[lin:lin+1]
+            race.append(lap_data)
+        x += 1
