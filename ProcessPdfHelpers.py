159e840f7c24d26cc951f478967f5f0f52b8b595
Neal Maze <nealwmaze@gmail.com>
2021-05-03 16:40:46 -0600

    y

--- ProcessPdfHelpers.py
+++ ProcessPdfHelpers.py
@@ -3,6 +3,84 @@ from os import listdir
 import pdfplumber as plumb
 import fnmatch
 
+def openPDF(rcFile):
+    pdf = plumb.open(rc_file)
+    pages = pdf.pages
+    const = getSessionConstants(pages)
+    sheets = getSheets(pages)
+
+    return sheets, const
+
+def getSessionConstants(pages):
+    words = pages[0].extract_words()
+    sess_const = []
+
+    year = words[-5]["text"]
+    day = words[-6]["text"]
+    month = words[-7]["text"]
+
+    date = f"{month} {day} {year}"
+    sess_const.append(date)
+    sess_const.append(year)
+    TRK = words[9]["text"]
+    sess_const.append(TRK)
+    league = words[8]["text"]
+    sess_const.append(league)
+    session = words[14]["text"]
+    sess_const.append(session)
+    return sess_const
+
+def getSheets(pages):
+    sheets = []
+    positions = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th",
+                 "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th",
+                 "28th", "29th", "30th", "31st", "32nd", "33rd", "34th", "35th", "36th", "37th", "38th", "39th", "40th"]
+
+    for pg in pages:
+        words = pg.extract_words()
+        txt = []
+        sheet = []
+        for i in words:
+            txt.append(i["text"])
+
+        stripBoilerPlate(txt)
+        for t in txt:
+            sheet.append(t)
+
+        x = 0
+        for i in sheet:
+            if sheet[x] == "*":
+                sheet[x-1] += " *"
+                del sheet[x]
+            elif sheet[x] == "P":
+                sheet[x-1] += " P"
+                del sheet[x]
+            else:
+                x += 1
+        sheets.append(sheet)
+
+    return sheets
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def parseRacAnalysis(rc_file):
     pdf = plumb.open(rc_file)
     pages = pdf.pages
@@ -66,25 +144,6 @@ def getRacAnalysis(yr, dir):
     filter_files = fnmatch.filter(listdir(dir), f"{yr}*RAC*nalysis.pdf")
     rcFiles = [f"{dir}/{file}" for file in filter_files]
     return rcFiles
-
-def getSessionConstants(pages):
-    words = pages[0].extract_words()
-    sess_const = []
-
-    year = words[-5]["text"]
-    day = words[-6]["text"]
-    month = words[-7]["text"]
-
-    date = f"{month} {day} {year}"
-    sess_const.append(date)
-    sess_const.append(year)
-    TRK = words[9]["text"]
-    sess_const.append(TRK)
-    league = words[8]["text"]
-    sess_const.append(league)
-    session = words[14]["text"]
-    sess_const.append(session)
-    return sess_const
 
 def getTxt(pages):
     sheets = []
