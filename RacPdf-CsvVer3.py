16081763752eedb67282819f9f558fcc37e53aec
Neal Maze <nealwmaze@gmail.com>
2021-05-08 20:41:30 -0600

    y

--- RacPdf-CsvVer3.py
+++ RacPdf-CsvVer3.py
@@ -4,7 +4,8 @@ from HelpersVer3 import *
 dir = ("C:/Users/LuciusFish/Desktop/MotoGP_PDFs/Analysis")
 yrs = ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014",
        "2013", "2012", "2011", "2010"]
-types = ["RAC", "RAC2", "Q2", "Q1", "WUP", "FP1", "FP2", "FP3", "FP4"]
+types = ["RAC2", "Q2", "Q1", "WUP", "FP1", "FP2", "FP3", "FP4"]
+finTypes = ["RAC"]
 
 mkGoalDirs()
 
@@ -27,7 +28,7 @@ for sesType in types:
     dest = (f"C:/Users/LuciusFish/Desktop/csv/{sesType}/")
 
     for yr in yrs:
-        rcFiles = getRacAnFiles(yr, dir, "RAC")
+        rcFiles = getRacAnFiles(yr, dir, sesType)
         print(f"{yr}, {sesType}")
 
         for file in rcFiles:
