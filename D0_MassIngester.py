c0b8ccb585ba564cab431093b942381b0e98fb5c
Neal Maze <nealwmaze@gmail.com>
2021-06-17 20:48:43 -0600

    working on MassIngester
    created SQL_Strings

--- D0_MassIngester.py
+++ D0_MassIngester.py
@@ -3,22 +3,38 @@ from sqlalchemy import create_engine
 import pandas as pd
 from lists import *
 from B2_ConverterHelpers import *
+import psycopg2
 
-engine = create_engine("postgresql://postgres:7158@localhost:5432/MotoGP")
+conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
+cur = con.cursor()
 
-for yr in yrs:
-    for lge in lges:
-        fLis = getFiles(csvSesDir, f"{yr}-{lge}*.csv")
 
-        if len(fLis) != 0:
-            print("")
-            print(f" - - - {yr}, {lge} - - - ")
 
-        for file in fLis:
-            with open(file, "r", encoding='utf-8') as f:
-                df = pd.read_csv(f)
-            df.to_sql("gp_times", con = engine, index = True, index_label = "index", if_exists = "append")
-            print(file)
 
-engine.dispose()
-print("\n\n   - - - data ingested - - -   \n\n")
+
+
+
+
+
+
+
+
+
+# engine = create_engine("postgresql://postgres:7158@localhost:5432/MotoGP")
+#
+# for yr in yrs:
+#     for lge in lges:
+#         fLis = getFiles(csvSesDir, f"{yr}-{lge}*.csv")
+#
+#         if len(fLis) != 0:
+#             print("")
+#             print(f" - - - {yr}, {lge} - - - ")
+#
+#         for file in fLis:
+#             with open(file, "r", encoding='utf-8') as f:
+#                 df = pd.read_csv(f)
+#             df.to_sql("gp_times", con = engine, index = True, index_label = "index", if_exists = "append")
+#             print(file)
+#
+# engine.dispose()
+# print("\n\n   - - - data ingested - - -   \n\n")
---
+++ SQL_Strings.py
@@ -0,0 +1,16 @@
+
+
+
+
+createTimesTable = """
+    CREATE TABLE accounts(
+        index integer PRIMARY Key,
+        month text,
+        day text,
+        yr integer,
+        lge text,
+        rnd integer,
+        session text,
+        
+    )
+"""
