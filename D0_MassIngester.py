d4b4b348b89392d53a1a65e5d078f685fb4cdfa2
Neal Maze <nealwmaze@gmail.com>
2021-06-24 14:18:09 -0600

    y

--- D0_MassIngester.py
+++ D0_MassIngester.py
@@ -7,40 +7,46 @@ import psycopg2
 import numpy
 from SQL_Strings import *
 
-# password = input("\n\nPassword: ")
-# print("\n\n")
-#
-# conn = psycopg2.connect(f"host=localhost dbname=postgres user=postgres password={password}")
-# cur = conn.cursor()
+password = input("\n\nPassword: ")
+print("\n\n")
 
-# cur.execute(createTimesTable)
-# conn.commit()
+engine = create_engine("postgresql://postgres:7158@localhost:5432/MotoGP")
+conn = psycopg2.connect(f"host=localhost dbname=MotoGP user=postgres password={password}")
+cur = conn.cursor()
 
-for yr in yrs[:1]:
-    for lge in lges[:1]:
+cur.execute(dropTimesTable)
+conn.commit()
+cur.execute(createTimesTable)
+conn.commit()
+
+for yr in yrs:
+    for lge in lges:
         fLis = getFiles(csvSesDir, f"{yr}-{lge}*.csv")
 
         if len(fLis) != 0:
             print("")
             print(f" - - - {yr}, {lge} - - - ")
 
-        for file in fLis[:1]:
+        for file in fLis:
+            print(file)
             with open(file, "r", encoding="utf-8") as f:
-                reader = csv.reader(f, delimiter = ",")
-                x = list(reader)
-                result = numpy.array(x).astype("str")
-                for row in result:
-                    print(row)
+                # reader = csv.reader(f, delimiter = ",")
+                # x = list(reader)
+                # result = numpy.array(x).astype("str")
+                # for row in result:
+                #     print(row)
 
+                df = pd.read_csv(f)
+                df.to_sql("gp_times", con = engine, if_exists = "append", index = False)
+
+                # conn.commit()
                 # frame = pd.read_csv(f)
                 # next(frame)
                 # cur.copy_from(f, "gp_times", sep=",")
 
-# conn.commit()
 
 
 
-# engine = create_engine("postgresql://postgres:7158@localhost:5432/MotoGP")
 #
 # for yr in yrs:
 #     for lge in lges:
--- SQL_Strings.py
+++ SQL_Strings.py
@@ -1,9 +1,12 @@
 
 
-
+dropTimesTable = """
+    DROP TABLE gp_times
+    """
 
 createTimesTable = """
     CREATE TABLE gp_times(
+    
         index text PRIMARY KEY,
         month text,
         day text,
@@ -12,6 +15,7 @@ createTimesTable = """
         rnd integer,
         session text,
         trk text,
+        
         pos text,
         rdr_num integer,
         f_name text,
@@ -20,22 +24,36 @@ createTimesTable = """
         team text,
         manu text,
         num_of_laps integer,
+        
         run_num integer,
         f_tire text,
         r_tire text,
         laps_on_f integer,
         laps_or_r integer,
         lap_num integer,
+        
         lap_time text,
-        pit text,
+        lap_seconds decimal,
+        lap_valid boolean,
+        
+        pit boolean,
+        
         sec_one text,
+        one_seconds decimal,
+        one_valid boolean,
+        
         sec_two text,
+        two_seconds decimal,
+        two_valid boolean,
+        
         sec_thr text,
+        thr_seconds decimal,
+        thr_valid boolean,
+        
         sec_four text,
-        sec_fiv text,
-        sec_six text,
-        sec_sev text,
-        sec_eig text,
+        four_seconds decimal,
+        four_valid boolean,
+        
         avg_spd text
         )
     """
