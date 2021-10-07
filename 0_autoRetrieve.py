25dd63a0e94d3eca3fa337aeb8da5c38d3bb0a05
Neal Maze <nealwmaze@gmail.com>
2021-10-07 02:37:52 -0600

    Moved User Input
    Users now set all parameters before program executes.

--- 0_autoRetrieve.py
+++ 0_autoRetrieve.py
@@ -2,8 +2,7 @@
 from retrieveHelpers import *
 
 print("\n          Data Retrieval System")
-print("          Step 1 - Session PDF Retrieval")
-
+print("          Step 1 - Set Parameters")
 # get year from user
 yr = "x"
 yrs = ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008",
@@ -12,38 +11,39 @@ yrs = ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "
 while yr not in yrs:
     yr = input("\nplease enter a year from 1998 to the present from which to retrieve files\nretrieve files from year: ")
     if yr not in yrs: print("invalid selection\n")
-
 inRnd = input('\nenter numerals of requested rounds, separated by \n'
               'a comma and a space, or request a range like "4-10"\nretrieve files from rounds: ')
+testConf = input("\ndo you wish to retrieve testing data? (y/n): ")
+uIn = input("do you wish to clean data in all seasons? (y/n): ")
 
+print("          Step 2 - Session PDF Retrieval")
 if ", " in inRnd:
     rnds = inRnd.split(", ")
 else: rnds = [inRnd]
 
+# retrieve PDF files
 grabFiles(yr, rnds)
 
 # get testing files
-print("\n          Step 2 - Test PDF Retrieval")
-testConf = input("\ndo you wish to retrieve testing data? (y/n): ")
+print("\n          Step 3 - Test PDF Retrieval")
 if testConf == "y":
     print("getting test PDFs from year")
     getTestFiles(yr)
 
 # convert PDFs to CSVs
-print("\n          Step 3 - PDF Conversion")
+print("\n          Step 4 - PDF Conversion")
 print(f"Converting {yr} session analysis files into csv files")
 convertYrPdfs(yr, rnds)
 print("converted")
 
 # convert grid files to CSVs
-print("\n          Step 4 - Grid Conversion")
+print("\n          Step 5 - Grid Conversion")
 print(f"Converting {yr} qualifying files into csv files")
 convertGrid
 print("converted")
 
 # clean data
-print("\n          Step 5 - Data Cleaning")
+print("\n          Step 6 - Data Cleaning")
 print(f"Converting {yr} pdf files into csv files")
-uIn = input("do you wish to clean data in all seasons? (y/n): ")
 cleanData(uIn, yr)
 print("cleaned")
